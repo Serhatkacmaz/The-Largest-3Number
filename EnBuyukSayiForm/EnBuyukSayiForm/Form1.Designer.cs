7237ebe3f1822c7853c83744472eecab639f5dca
Serhatkacmaz <56757412+Serhatkacmaz@users.noreply.github.com>
2021-11-06 01:43:05 +0200

    code

---
+++ EnBuyukSayiForm/EnBuyukSayiForm/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace EnBuyukSayiForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonFindNumber = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.input6 = new System.Windows.Forms.TextBox();
+            this.input5 = new System.Windows.Forms.TextBox();
+            this.input4 = new System.Windows.Forms.TextBox();
+            this.input3 = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1. Sayı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2. Sayı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(55, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "3. Sayı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(269, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "4. Sayı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(269, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "6. Sayı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(269, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "5. Sayı";
+            // 
+            // buttonFindNumber
+            // 
+            this.buttonFindNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFindNumber.Location = new System.Drawing.Point(137, 249);
+            this.buttonFindNumber.Name = "buttonFindNumber";
+            this.buttonFindNumber.Size = new System.Drawing.Size(238, 23);
+            this.buttonFindNumber.TabIndex = 6;
+            this.buttonFindNumber.Text = "Bul";
+            this.buttonFindNumber.UseVisualStyleBackColor = true;
+            this.buttonFindNumber.Click += new System.EventHandler(this.buttonFindNumber_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(131, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(244, 31);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "En Büyük 3.  Sayı";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(137, 306);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(238, 23);
+            this.buttonExit.TabIndex = 8;
+            this.buttonExit.Text = "Çıkış";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // input6
+            // 
+            this.input6.Location = new System.Drawing.Point(340, 189);
+            this.input6.Name = "input6";
+            this.input6.Size = new System.Drawing.Size(70, 20);
+            this.input6.TabIndex = 5;
+            // 
+            // input5
+            // 
+            this.input5.Location = new System.Drawing.Point(340, 152);
+            this.input5.Name = "input5";
+            this.input5.Size = new System.Drawing.Size(70, 20);
+            this.input5.TabIndex = 4;
+            // 
+            // input4
+            // 
+            this.input4.Location = new System.Drawing.Point(340, 116);
+            this.input4.Name = "input4";
+            this.input4.Size = new System.Drawing.Size(70, 20);
+            this.input4.TabIndex = 3;
+            // 
+            // input3
+            // 
+            this.input3.Location = new System.Drawing.Point(126, 188);
+            this.input3.Name = "input3";
+            this.input3.Size = new System.Drawing.Size(70, 20);
+            this.input3.TabIndex = 2;
+            // 
+            // input2
+            // 
+            this.input2.Location = new System.Drawing.Point(126, 151);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(70, 20);
+            this.input2.TabIndex = 1;
+            // 
+            // input1
+            // 
+            this.input1.Location = new System.Drawing.Point(126, 115);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(70, 20);
+            this.input1.TabIndex = 0;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.Location = new System.Drawing.Point(137, 278);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(238, 23);
+            this.buttonClear.TabIndex = 7;
+            this.buttonClear.Text = "Temizle";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(483, 341);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.buttonFindNumber);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input6);
+            this.Controls.Add(this.input3);
+            this.Controls.Add(this.input5);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input4);
+            this.Controls.Add(this.input1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sayı Girişleri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonFindNumber;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.TextBox input6;
+        private System.Windows.Forms.TextBox input5;
+        private System.Windows.Forms.TextBox input4;
+        private System.Windows.Forms.TextBox input3;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ EnBuyukSayiForm/EnBuyukSayiForm/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnBuyukSayiForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList listNumber = new ArrayList();
+
+        private Boolean isNotEmpty()
+        {
+            if (input1.Text != "" && input2.Text != "" && input3.Text != "" && input4.Text != "" && input5.Text != ""
+                && input6.Text != "")
+                return true;
+            return false;
+        }
+
+
+        private void buttonFindNumber_Click(object sender, EventArgs e)
+        {
+            if (isNotEmpty())
+            {
+                listNumber.Clear();
+                listNumber.Add(Int16.Parse(input1.Text));
+                listNumber.Add(Int16.Parse(input2.Text));
+                listNumber.Add(Int16.Parse(input3.Text));
+                listNumber.Add(Int16.Parse(input4.Text));
+                listNumber.Add(Int16.Parse(input5.Text));
+                listNumber.Add(Int16.Parse(input6.Text));
+                listNumber.Sort();
+                listNumber.Reverse();
+                MessageBox.Show("En büyük 3. sayı: " + listNumber[2].ToString());
+            }
+            else
+            {
+                MessageBox.Show("Eksik sayılar var.");
+            }
+        }
+
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            input1.Text = "";
+            input2.Text = "";
+            input3.Text = "";
+            input4.Text = "";
+            input5.Text = "";
+            input6.Text = "";
+        }
+    }
+}
